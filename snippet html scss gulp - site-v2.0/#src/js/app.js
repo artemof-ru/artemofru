ff2fb64ab1c26a2cc2d990c88cb171f804164417
artemofru <88113236+artemofru@users.noreply.github.com>
2021-08-05 22:19:50 +0600

    Add files via upload

---
+++ snippet html scss gulp - site-v2.0/#src/js/app.js
@@ -0,0 +1,26 @@
+//======================================================================================
+//Тест на поддержеку браузера - изображений webP
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
+  }
+});
+
+
+//======================================================================================
+//Всплывающие окна
+@@include('_popup.js');
+
+
+//======================================================================================
+// Видео с youtube
+@@include('_video.js');
